42213c19c0250ff82f0f71ee5369cb56183dd66e
go19kul16 <go19kul16.gmail.com>
2025-01-31 12:29:34 +0530

    second commit

---
+++ src/main/java/com/gokul/MyDemoProjects/App.java
@@ -0,0 +1,13 @@
+package com.gokul.MyDemoProjects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
